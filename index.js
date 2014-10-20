dd2092fd157721abbbcacae01b5034bbc3ca3b86
Andrew Ingram <andy@andrewingram.net>
2014-10-20 18:46:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ module.exports = function(source) {
   var current = loaderUtils.getCurrentRequest(this);
 
   var query = loaderUtils.parseQuery(this.query);
-  if (query.insertPragma) {
-    source = '/** @jsx ' + query.insertPragma + ' */' + source;
-  }
 
   var transform = reactTools.transformWithDetails(source, {
     harmony: query.harmony,
